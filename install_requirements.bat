a65249eac994a8e2e87a6d4a61bfd2126cfe660c
pedrolazzaroni <phlazzaroni@gmail.com>
2025-03-19 10:47:44 -0300

    Reset

--- install_requirements.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Instalando dependências do LazzFit...
-pip install -r requirements.txt
-echo.
-echo Instalação concluída! Pressione qualquer tecla para fechar esta janela.
-pause > nul
